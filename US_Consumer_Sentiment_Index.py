5ef71a9b8a779e78a63e539c729bd218d5a4bc0e
krizhnaa <krizhnaprem@gmail.com>
2024-07-06 12:03:10 +0530

    Added 20 Data Streams

---
+++ US_Consumer_Sentiment_Index.py
@@ -0,0 +1,25 @@
+# US Consumer Sentiments Index
+
+import requests
+import json
+import datetime as dt
+
+url = "https://api.stlouisfed.org/fred/series/observations?series_id=UMCSENT&api_key=7ef44306675240d156b2b8786339b867&file_type=json"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = json.loads(response.text)
+    observations = data['observations']
+    result = {}
+    for obs in observations:
+        date = dt.datetime.strptime(obs['date'], '%Y-%m-%d')
+        formatted_date = date.strftime('%Y-%m-%d %H:%M:%S.%f')
+        try:
+            value = float(obs['value'])
+            result[formatted_date] = value
+        except ValueError:
+            continue
+    print(result)
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
+
